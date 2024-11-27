862a3576cedfbbce368edb3d99da310669bc6c74
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-11-27 22:07:20 +0100

    Update main.cpp
    Updated for changed functions (interpolation)

--- interpolation/main.cpp
+++ interpolation/main.cpp
@@ -2,24 +2,29 @@
 // Parameters can be changed in 
 // img is interlaced, grayscale, uint8 image.  
 
-// Determine rotaion coordinates
-array<float, 3> parameters = coordinateLSQ(img);
-//array<float, 3> parameters = { 373.468, -240.483, - float(1.50362 * pi / 180) };
+bool lensCorr = true;
 
+Mat img = imReadGS("img_test01.png");
+Mat img_nr = RemoveSmallParticleNoise(img);
+
+// Determine rotaion coordinates
+parameters = coordinateLSQ(img_nr);
+if (parameters[0] == 0) {
+    // Default paramters if LSQ doesnt work
+    parameters = { 373.468, -240.483, -float(1.50362 * pi / 180) };
+}
+    
 // Simple image rotation
-// Mat img_shift = imShiftSimple(img, parameters);
+Mat img_shift = imShiftSimple(img_nr, parameters);
 
 // Interpolation
-Mat img_coord = imShiftCoord(img, parameters);
-Mat img_intp = interpolateNN(img_coord, 8);
-// Mat img_intp = imread("intp_tmp.png", IMREAD_GRAYSCALE);
+Mat img_coord = imShiftCoord(img_nr, parameters, lensCorr);
+Mat img_intp = interpolateNN(img_coord, 4, lensCorr);
+//Mat img_intp = imread("intp_tmp.png", IMREAD_GRAYSCALE);
 imwrite("intp_tmp.png", img_intp);  // Save file for quick load when testing other functions 
 
-// Flux
+// Print flux values of objects in image
 blobFlux(img_intp, 40, 15);
 
-// Plot intensities
-intensityPlot(img_intp, 182, 404);
-intensityPlot(img_intp, 518, 373);
-
-    
+// Intensity plot of given coordinate (x,y)
+intensityPlot(img_intp, 498, 304);
