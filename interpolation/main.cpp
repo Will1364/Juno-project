4011ce6756006c8a3cc87ebb3d3bda997ee0fc9c
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-11-27 11:37:24 +0100

    Update main.cpp

--- interpolation/main.cpp
+++ interpolation/main.cpp
@@ -3,14 +3,14 @@
 // img is interlaced, grayscale, uint8 image.  
 
 // Determine rotaion coordinates
-parameters = coordinateLSQ(img);
-//parameters = { 373.468, -240.483, - float(1.50362 * pi / 180) };
+array<float, 3> parameters = coordinateLSQ(img);
+//array<float, 3> parameters = { 373.468, -240.483, - float(1.50362 * pi / 180) };
 
 // Simple image rotation
-// Mat img_shift = imShiftSimple(img_nr, parameters);
+// Mat img_shift = imShiftSimple(img, parameters);
 
 // Interpolation
-Mat img_coord = imShiftCoord(img_nr, parameters);
+Mat img_coord = imShiftCoord(img, parameters);
 Mat img_intp = interpolateNN(img_coord, 8);
 // Mat img_intp = imread("intp_tmp.png", IMREAD_GRAYSCALE);
 imwrite("intp_tmp.png", img_intp);  // Save file for quick load when testing other functions 
