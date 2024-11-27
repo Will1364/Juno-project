dac02cd40c0d34cd27ae940871dbf606821b397a
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-11-27 22:07:56 +0100

    Update main.cpp
    Fixed small error from previous upload

--- interpolation/main.cpp
+++ interpolation/main.cpp
@@ -4,21 +4,18 @@
 
 bool lensCorr = true;
 
-Mat img = imReadGS("img_test01.png");
-Mat img_nr = RemoveSmallParticleNoise(img);
-
 // Determine rotaion coordinates
-parameters = coordinateLSQ(img_nr);
+parameters = coordinateLSQ(img);
 if (parameters[0] == 0) {
     // Default paramters if LSQ doesnt work
     parameters = { 373.468, -240.483, -float(1.50362 * pi / 180) };
 }
     
 // Simple image rotation
-Mat img_shift = imShiftSimple(img_nr, parameters);
+Mat img_shift = imShiftSimple(img, parameters);
 
 // Interpolation
-Mat img_coord = imShiftCoord(img_nr, parameters, lensCorr);
+Mat img_coord = imShiftCoord(img, parameters, lensCorr);
 Mat img_intp = interpolateNN(img_coord, 4, lensCorr);
 //Mat img_intp = imread("intp_tmp.png", IMREAD_GRAYSCALE);
 imwrite("intp_tmp.png", img_intp);  // Save file for quick load when testing other functions 
