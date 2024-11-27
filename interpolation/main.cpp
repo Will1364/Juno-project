af5c0225ca2ca7c35bc9b8e76db16a5f95bc0687
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-11-27 11:23:32 +0100

    Update main.cpp

--- interpolation/main.cpp
+++ interpolation/main.cpp
@@ -3,11 +3,23 @@
 // img is interlaced, grayscale, uint8 image.  
 
 // Determine rotaion coordinates
-array<float, 3> parameters = coordinateLSQ(img);
-   
-// Simple image despin
-Mat img_shift = imShiftSimple(img, parameters);
+parameters = coordinateLSQ(img);
+//parameters = { 373.468, -240.483, - float(1.50362 * pi / 180) };
 
-// Despin by interpolation
-Mat img_coord = imShiftCoord(img, parameters);
+// Simple image rotation
+// Mat img_shift = imShiftSimple(img_nr, parameters);
+
+// Interpolation
+Mat img_coord = imShiftCoord(img_nr, parameters, true);
 Mat img_intp = interpolateNN(img_coord, 8);
+// Mat img_intp = imread("intp_tmp.png", IMREAD_GRAYSCALE);
+imwrite("intp_tmp.png", img_intp);  // Save file for quick load when testing other functions 
+
+// Flux
+blobFlux(img_intp, 40, 15);
+
+// Plot intensities
+intensityPlot(img_intp, 182, 404);
+intensityPlot(img_intp, 518, 373);
+
+    
