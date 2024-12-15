4e6f94da1db8652d5126f10a6408da4cf57b7666
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-12-15 22:38:05 +0100

    Create main.cpp
    Code updated for float images

---
+++ interpolation_float/main.cpp
@@ -0,0 +1,33 @@
+// Copy below to use interpolation functions. 
+// Parameters can be changed in 
+// img is interlaced, grayscale, CV_32FC1 image with values [0, 1]
+// 
+
+
+bool lensCorr = true;
+
+// Determine rotaion coordinates
+array<float, 3> parameters = { 0, 0, 0 };
+parameters = coordinateLSQ(img);
+if (parameters[0] == 0) {
+    // Default paramters if LSQ doesnt work
+    parameters = { 376, -240, -float(1.4968 * pi / 180.0) };
+}
+    
+// Simple image rotation
+Mat img_shift = imShiftSimple(img, parameters);
+
+// Interpolation
+Mat img_coord = imShiftCoord(img, parameters, lensCorr);
+Mat img_intp = interpolateNN(img_coord, 4, lensCorr);
+//Mat img_intp = imread("intp_tmp.png", IMREAD_GRAYSCALE);
+imwrite("intp_tmp.png", img_intp);  // Save file for quick load when testing other functions 
+
+// Print flux values of objects in image
+blobFlux(img_intp, 40, 15);
+
+// Intensity plot of given coordinate (x,y)
+intensityPlot(img_intp, 498, 304);
+
+// Create upscaled version of star (~260 * 260 image)
+Mat img_starScaled = starPlot(img_intp, 189, 407, 8);
