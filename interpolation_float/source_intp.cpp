dbd81d3d5398335ce797a9db77e01929cbd11009
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-12-17 09:51:34 +0100

    Update source_intp.cpp
    Added angular interpolation

--- interpolation_float/source_intp.cpp
+++ interpolation_float/source_intp.cpp
@@ -10,6 +10,8 @@
 
 // Interpolation
 Mat interpolateNN(Mat img, int arr_size, bool lensCorrection);
+Mat interpolateNNAng(Mat img, int arr_size);
+
 
 // Interpolation cores
 double intpCoreNNPlus(vector<float> min_d, vector<float> min_v, int max_dist);
@@ -208,6 +210,249 @@ Mat interpolateNN(Mat img, int arr_size, bool lensCorrection) {
 	return interpolated;
 }
 
+
+Mat interpolateNNAng(Mat img, int arr_size) {
+	// Use Mat from imShiftCoordAng
+
+	// Variables : 
+	int search = 50;		// NxN search area around each pixel. 50 is a good balance between compute time and extra buffer 
+	int padding = 0;		// Number of lines in original image to not include
+	float max_dist = 9999;	// The furthest away 'closest' pixel can be 
+	float min_dist = 0.00;  // If min distance is closer than this, uses uninterpolated value. Set to zero or negative to disable
+
+	// Parameter setup
+	int width = img.cols;
+	int height = img.rows;
+
+	int width_i = width - padding;
+	int height_i = height - padding;
+
+	int s_x = 0;
+	int s_y = 0;
+
+	double theta_org = 0;
+	double rho_org = 0;
+	double theta_intp = 0;
+	double rho_intp = 0;
+
+
+	double dist = 0;
+
+	vector<float> min_d(arr_size, 0);
+	vector<float> min_v(arr_size, 0);
+
+	vector<int> min_i(arr_size, 0);
+	vector<int> min_j(arr_size, 0);
+
+	double val = 0;
+	double tmp = 0;
+
+	Mat interpolated(height_i, width_i, CV_32FC1);
+	interpolated = 0;
+
+	// Bellow are 'niceness' parameters, used for debugging / to track progress
+	Mat vals_used(height, width, CV_8UC1);		// Mat used to display pixels used. Can be commented out without affecting the interpolation
+	vals_used = 250;
+
+	int used = 0;		// Counts used pixels from even rows
+	int unused = 0;		// Counts number of unused pixels
+	int progress = 1;	// Used to display progress
+	int count_mindist = 0;
+
+
+
+	// Lens correction
+	float dx = 8.6;			// Physical pixel size [µm]
+	float dy = 8.3;
+
+	int x_l0 = 383 - border_pad;			// Radial distortion center (from Matlab script)
+	int y_l0 = 257 - border_pad;
+
+	double efl = 20006;		// Effective focal length
+	double f = efl / dx;	// focal length
+
+	double r = 0;
+	float xt = 0;							// Temporary coord
+	float yt = 0;
+
+	Mat vec(1, 3, CV_64FC1);
+	vec = 0;
+
+	Mat R_fSC = eulerRot(1, 2, 3, 167.387, 0.4255, -0.3141, true);
+
+	// Angular coordinate grid 
+	Mat coordGrid(height_i, width_i, CV_64FC2);
+
+	for (int i = 0; i < height_i; i++) {
+		for (int j = 0; j < width_i; j++) {
+
+			xt = j;
+			yt = i;
+
+			// Radial lens correction
+			xt -= x_l0;
+			yt -= y_l0;
+
+			yt = yt * dy / dx;
+
+			xt = xt / f;
+			yt = yt / f;
+
+			// CCD coordinates
+			r = sqrt(xt * xt + yt * yt + 1);
+			xt = xt / r;
+			yt = yt / r;
+
+			vec.at<double>(0, 0) = xt;
+			vec.at<double>(0, 1) = yt;
+			vec.at<double>(0, 2) = 1 / r;
+
+			vec = vec * R_fSC;
+
+			coordGrid.at<Vec2d>(i, j)[0] = atan2(vec.at<double>(0, 1), vec.at<double>(0, 0));
+			coordGrid.at<Vec2d>(i, j)[1] = acos(-vec.at<double>(0, 2));
+
+		}
+	}
+
+
+
+	// Interpolation function
+	cout << "interpolating";
+	// Loops through all image values
+	for (int i = 0; i < height_i; i++) {
+		for (int j = 0; j < width_i; j++) {
+
+			// Reset paramters 
+			for (int k = 0; k < arr_size; k++) {
+				min_d[k] = 9990;
+				min_v[k] = 0;
+			}
+
+			// Search center coordinate (in input image)  
+			s_y = i - (search / 2); // -y_l0 * lensCorrection;
+			s_x = j - (search / 2); // -x_l0 * lensCorrection;
+
+			theta_intp = coordGrid.at<Vec2d>(i, j)[0];
+			rho_intp = coordGrid.at<Vec2d>(i, j)[1];
+
+			// Fills search window with distance to point in a search x search grid around 
+			// Finds nearest arr_size values within search window 
+			for (int k = 0; k < search; k++) {
+				for (int l = 0; l < search; l++) {
+
+					// Test if search coordinate is within input image 
+					if (s_y + k > padding and s_y + k < height - padding and s_x + l > padding and s_x + l < width - padding) {
+
+						// Calculate distance
+						theta_org = img.at<Vec3d>(s_y + k, s_x + l)[0];
+						rho_org = img.at<Vec3d>(s_y + k, s_x + l)[1];
+
+						//dist = acos(sin(theta_org)* sin(theta_intp) + cos(theta_org)*cos(theta_intp)*cos(abs(rho_org-rho_intp)));
+						dist = acos(sin(rho_org) * sin(rho_intp) + cos(rho_org) * cos(rho_intp) * cos(abs(theta_org - theta_intp)));
+
+
+						// Update arrays if with closest values 
+						for (int m = 0; m < arr_size; m++) {
+							if (min_d[m] > dist) {
+								for (int n = arr_size - 1; n > m; n--) {
+									min_d[n] = min_d[n - 1];
+									min_i[n] = min_i[n - 1];
+									min_j[n] = min_j[n - 1];
+								}
+
+								min_d[m] = dist;
+								min_i[m] = s_y + k;
+								min_j[m] = s_x + l;
+
+								break;
+							}
+
+						}
+
+
+
+					}
+
+				}
+			}
+
+
+			// Count to check that even and uneven rows from intput image are used roughly equally
+			if (min_i[0] % 2 == 0) {
+				used += 1;
+			}
+
+			// Uses uninterpolated value if pixel is closer than min_dist
+			if (min_d[0] < min_dist) {
+				interpolated.at<float>(i, j) = img.at<Vec3d>(min_i[0], min_j[0])[2];
+				count_mindist++;
+			}
+
+			else if (min_d[0] < max_dist) {
+
+				// Save values for closest pixels
+				for (int k = 0; k < arr_size; k++) {
+					min_v[k] = img.at<Vec3d>(min_i[k], min_j[k])[2];
+				}
+
+				// 
+				// Interpolation cores: Seperate functions where interpolation calculations are done
+				// Currently made: 
+				// Nearest neighbour: Just closest value, val = min_v[0]
+				// Inverse distance weighting: intpCoreDist(min_d, min_v), weight by 1/(d**p), with p being weighing parameter
+				// Radial basis interpolation: intpCoreRadialBasis(j, i, min_d, min_i, min_j, min_v, img), see function description
+				// 
+
+				// 
+				val = min_v[0];															// Nearest neighbour
+				// val = intpCoreNNPlus(min_d, min_v, max_dist);							// NN+; Takes average of all value within radius of max_dist (1, set in core).  					
+				// val = intpCoreDist(min_d, min_v);										// Inverse distance weighting 
+				// val = intpCoreRadialBasis(j, i, min_d, min_i, min_j, min_v, img);		// Radial basis interpolation
+
+
+				// Put value into interpolated image 
+				interpolated.at<float>(i, j) = val;
+
+				// Count pixels used (optional, can be commented out)
+				vals_used.at<uchar>(min_i[0], min_j[0]) -= 50;
+
+			}
+			else {
+				// If closest pixel is further away 
+				interpolated.at<float>(i, j) = 0;
+				unused++;
+			}
+
+			// Progress bar, can be commented out
+			if (i > progress * height / 10) {
+				progress++;
+				cout << ".";
+			}
+
+			// Reset parameters
+			val = 0;
+			tmp = 0;
+
+		}
+	}
+
+	// display val_used image
+	//namedWindow("Image intp shift vals used", WINDOW_AUTOSIZE);
+	//imshow("Image intp shift vals used", vals_used);
+	//imwrite("img_intp_used.png", vals_used);
+
+	// Output text at function end 
+	cout << "\rinterpolated                       " << endl;
+	cout << "fraction used from each segment: " << float(used) / (height_i * width_i) << endl;
+	cout << "fraction unused: " << float(unused) / (height_i * width_i) << endl;
+	cout << "fraction closer than min_dist (" << min_dist << "): " << float(count_mindist) / (height_i * width_i) << endl;
+	cout << endl;
+
+	return interpolated;
+}
+
+
 double intpCoreDist(vector<float> min_d, vector<float> min_v) {
 	// Inverse distance weight for interpolation
 	// Weights each value with 1/(d**p), d being distance to pixel, p being power 
@@ -271,6 +516,8 @@ double intpCoreRadialBasis(int x_, int y_, vector<float> min_d, vector<int> min_
 		f.at<double>(i, 0) = min_v[i];
 
 	}
+
+	// solve(A, b, x, DECOMP_SVD);
 
 	Mat w = phi.inv() * f;		// Determine weights
 	double s = 0;				// Intp value
