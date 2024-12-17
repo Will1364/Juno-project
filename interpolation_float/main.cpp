bbed902d513e623826e2f8a6ef63a3729c65fe38
Schejbel <114143628+Schejbel@users.noreply.github.com>
2024-12-17 09:53:12 +0100

    Update main.cpp
    Updated with two new functions

--- interpolation_float/main.cpp
+++ interpolation_float/main.cpp
@@ -20,7 +20,13 @@ Mat img_shift = imShiftSimple(img, parameters);
 // Interpolation
 Mat img_coord = imShiftCoord(img, parameters, lensCorr);
 Mat img_intp = interpolateNN(img_coord, 4, lensCorr);
-imwrite("intp_tmp.png", img_intp * 255.0);  // Save file for quick load when testing other functions 
+imwrite("_intp.png", img_intp * 255.0);  // Save file for quick load when testing other functions 
+
+// Angular interpolation 
+Mat img_coordAng = imShiftCoordAng(img);
+Mat img_intpAng = interpolateNNAng(img_coordAng, 1);
+imwrite("_intpAng.png", img_intpAng * 255.0);  // Save file for quick load when testing other functions 
+
 
 // Print flux values of objects in image
 blobFlux(img_intp, 40/255.0, 15/255.0);
